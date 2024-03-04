3677b41c6face395acba4a8ae0ece6e499190e57
vnazar <vnazarf@gmail.com>
2024-03-04 09:42:31 -0300

    v1.4

--- src/main/java/cl/vc/arb/scalping/front/controller/PrincipalController.java
+++ src/main/java/cl/vc/arb/scalping/front/controller/PrincipalController.java
@@ -33,10 +33,11 @@ public class PrincipalController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         try {
+              /*
             Repository.setPrincipalController(this);
             Notifier.startNotification();
             client = SplashController.actorSystem.actorOf(new RoundRobinPool(1).props(ClientActor.props()), "ClientManager");
-            /*
+
             Repository.setNettyProtobufClient(new NettyProtobufClient(SplashController.properties.getProperty("controller"),
                     client, "log/", "front-blotter", NotificationMessage.Component.valueOf(SplashController.properties.getProperty("name"))));
             new Thread(Repository.getNettyProtobufClient()).start();
